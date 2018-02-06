1335aa5e90eca6400846076bdb9187168a8fd892
Rudy7 <jyw4891@naver.com>
2018-02-06 20:22:22 +0900

    오후 8:22 2018-02-06

--- Kakao/app/src/main/java/com/bitcamp/app/kakao/activity/MemberDetail.java
+++ Kakao/app/src/main/java/com/bitcamp/app/kakao/activity/MemberDetail.java
@@ -1,7 +1,11 @@
 package com.bitcamp.app.kakao.activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.bitcamp.app.kakao.R;
 
@@ -11,5 +15,13 @@ public class MemberDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.member_detail);
+        final Context context = MemberDetail.this;
+        findViewById(R.id.update_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context,"수정",Toast.LENGTH_LONG).show();
+                startActivity(new Intent(context,MemberList.class));
+            }
+        });
     }
 }
--- Kakao/app/src/main/java/com/bitcamp/app/kakao/activity/MemberList.java
+++ Kakao/app/src/main/java/com/bitcamp/app/kakao/activity/MemberList.java
@@ -1,5 +1,6 @@
 package com.bitcamp.app.kakao.activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,15 +15,19 @@ public class MemberList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.member_list);
-
+        final Context context = MemberList.this;
+        findViewById(R.id.friend_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context,"친구프로필 보기",Toast.LENGTH_LONG).show();
+                startActivity(new Intent(context,MemberDetail.class));
+            }
+        });
         findViewById(R.id.logout_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-             /*   Toast.makeText(context,Toast.LENGTH_LONG).show();
-                Toast.makeText(context,"아이디"+ String.valueOf(.getText()),Toast.LENGTH_LONG).show();*/
-//                Intent intent = new Intent(context, MemberDetail.class);
-//                startActivity(intent);
-
+                Toast.makeText(context, "로그아웃",Toast.LENGTH_LONG).show();
+                startActivity(new Intent(context,Index.class));
             }
         });
     }
